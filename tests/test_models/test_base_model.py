53837a90e8b86203ef232be2f870c431ed4a0624
Mutisomary <marymutso2016@gmail.com>
2023-08-11 12:40:57 -0700

    tests

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -2,7 +2,7 @@
 """ A module to test base class"""
 import unittest
 from models.base_model import BaseModel
-from datetime import datetime
+from datetime import datetime, timedelta
 
 
 class TestBaseModel(unittest.TestCase):
@@ -73,6 +73,46 @@ class TestBaseModel(unittest.TestCase):
         obj1 = BaseModel()
         obj2 = BaseModel()
         self.assertNotEqual(obj1.id, obj2.id)
+
+    def test_created_at_after_init(self):
+        """Test if created_at is set correctly at initialization"""
+        now = datetime.now()
+        obj = BaseModel()
+        time_difference = now - obj.created_at
+        self.assertLessEqual(time_difference, timedelta(seconds=1))
+
+    def test_updated_at_after_init(self):
+        """Test if updated_at is set correctly at initialization"""
+        now = datetime.now()
+        obj = BaseModel()
+        time_difference = now - obj.updated_at
+        self.assertLessEqual(time_difference, timedelta(seconds=2))
+
+    def test_save_updates_updated_at(self):
+        """Test if save method updates updated_at"""
+        obj = BaseModel()
+        original_updated_at = obj.updated_at
+        obj.save()
+        self.assertNotEqual(obj.updated_at, original_updated_at)
+
+    def test_to_dict_structure(self):
+        """Test the structure of the dictionary returned by to_dict"""
+        obj = BaseModel()
+        obj_dict = obj.to_dict()
+        self.assertIsInstance(obj_dict, dict)
+        self.assertIn('__class__', obj_dict)
+        self.assertIn('id', obj_dict)
+        self.assertIn('created_at', obj_dict)
+        self.assertIn('updated_at', obj_dict)
+
+    def test_to_dict_values(self):
+        """Test the values of the dictionary returned by to_dict"""
+        obj = BaseModel()
+        obj_dict = obj.to_dict()
+        self.assertEqual(obj_dict['__class__'], 'BaseModel')
+        self.assertEqual(obj_dict['id'], obj.id)
+        self.assertEqual(obj_dict['created_at'], obj.created_at.isoformat())
+        self.assertEqual(obj_dict['updated_at'], obj.updated_at.isoformat())
 
 
 if __name__ == '__main__':
